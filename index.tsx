7bc1341e3ad91fc037852c8ab9e25ef6279ae024
AFMcGregor <alicemcgregor@live.com>
2025-09-04 20:46:00 +0100

    removed legacy api key (deleted previously anyway)

--- index.tsx
+++ index.tsx
@@ -10,17 +10,14 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+type firebaseConfigType = {
+    apiKey: string, authDomain: string, projectId: string, storageBucket: string, messagingSenderId: string,appId: string, measurementId: string
+}
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC0-KocgC9mlE57vQsfBU-7Yy2LYyh6Pxw",
-  authDomain: "what-even-is-web-accessibility.firebaseapp.com",
-  projectId: "what-even-is-web-accessibility",
-  storageBucket: "what-even-is-web-accessibility.appspot.com",
-  messagingSenderId: "343315310021",
-  appId: "1:343315310021:web:c2ea02d456cf443b6017ac",
-  measurementId: "G-7WWW4WCKKN",
-};
+const firebaseConfig: firebaseConfigType = process.env.firebaseConfig as unknown as firebaseConfigType;
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
